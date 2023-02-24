ff366150740b9d26344d0d26cf6da85bfba0f870
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2023-02-24 15:20:06 +0900

    add: practice_1 / AtCoder Practice 1を回答   package.json:    typescript用にファイルを設定
      src/atcoder/abs/*
        Beginers Contest 1用

---
+++ src/atcoder/abs/practice_1.ts
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+
+// util for input
+// prettier-ignore
+const lineit = (function* () { for (const line of fs.readFileSync(process.stdin.fd, "utf8").split("\n")) yield line.trim(); })();
+// prettier-ignore
+const wordit = (function* () { while (true) { let line = lineit.next(); if (line.done) break; for (const word of String(line.value).split(" ")) yield word; } })();
+// prettier-ignore
+const read = () => String((wordit.next()).value);
+
+// main
+const main = function () {
+    // TODO edit this code, this code is for https://atcoder.jp/contests/practice/tasks/practice_1
+
+    // param
+    let a: number = Number(read());
+    let b: number = Number(read());
+    let c: number = Number(read());
+    let s: string = read();
+
+	console.log('$a $b $c $s')
+	let sum = a + b + c;
+    // solve
+    let ans = `${sum} ${s}`;
+
+    // answer
+    console.log(ans);
+
+    return;
+};
+main();
