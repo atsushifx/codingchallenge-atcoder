945460fe4d821a19cf710a4dc9752ea4c668c038
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2023-03-05 17:24:26 +0900

    add: 01-Product: typescriptの回答を提出

---
+++ atcoder/abs/01-abc086a/main.ts
@@ -0,0 +1,72 @@
+/*
+ * atcoder typescript template
+ */
+import * as fs from "fs";
+
+// stdin
+const inputs: string = (fs.readFileSync(process.stdin.fd, "utf8"))
+const inputArray: string[] = inputs.split(/\s/)
+let inputIndex = 0;
+
+// atcoder lib:stdin
+function getNext(): string {
+	return inputArray[inputIndex++];
+}
+
+function getNextNum(): number {
+	return Number(getNext());
+}
+
+function getNums(N: number): number[] {
+	const arr: number[] = [];
+	for (let i: number = 0; i < N; ++i) {
+		arr[i] = getNextNum();
+	}
+	return arr;
+}
+
+// lib:stdout
+let outputBuffer: string = ""
+function print(out: string | number | bigint): void;
+function print<T>(out: Array<T>, separator: string): void;
+function print<T>(out: string | number | bigint | Array<T>, separator?: string) {
+	if (Array.isArray(out)) {
+		outputBuffer += out.join(separator);
+	} else {
+		outputBuffer += out;
+	}
+}
+
+function println(out: string | number | bigint): void;
+function println<T>(out: Array<T>, separator: string): void;
+function println<T>(out: string | number | bigint | Array<T>, separator?: string) {
+	if (Array.isArray(out)) {
+		print(out, separator || "");
+	} else {
+		print(out);
+	}
+	print("\n");
+}
+
+function flush(): void {
+	console.log(outputBuffer);
+}
+
+// exec main
+main();
+flush();
+
+
+// main : write answer in this function
+function main() {
+	// Input
+	const [a, b] = getNums(3);
+
+	// Answer
+	const Evens: string[] = ["Even", "Odd"];
+	const idx: number = (a % 2) * (b % 2)
+
+	// Out
+	print(Evens[idx]);
+}
+
