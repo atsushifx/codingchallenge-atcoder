0b7f6de3b22ba8ad86e4a834fd61745eb2641511
Furukawa Atsushi <atsushifx@gmail.com>
2024-09-08 07:04:15 +0900

    add: setting.json: code runner 用の設定を追加

---
+++ hackerrank/00-prepare/01-SolveMeFirst/ruby/main.rb
@@ -0,0 +1,11 @@
+# Copyright (c) 2024 Furukawa, Atsushi <atsushifx@gmail.com>
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+sum = a + b
+
+print "#{sum}"
+
